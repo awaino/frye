1dcdaa17e6cfada23be284f71fe6fc6a93dd9015
awaino <awaino@yahoo.com>
2016-12-07 18:45:26 -0700

    Changed menu slightly to stay in a loop until option to exit is pushed.

--- final/src/pkgfinal/Driver.java
+++ final/src/pkgfinal/Driver.java
@@ -21,40 +21,43 @@ public class Driver {
     }
  
     public static void menu(Word w){
-        System.out.println("Final Project Menu\n");
-        System.out.println("Press 1 for the Wordlist\n");
-        System.out.println("Press 2 for the Doclist\n");
-        System.out.println("Press 3 for Occurs With\n");
-        System.out.println("Press 4 to quit\n");
-        
-        Scanner fromUser = new Scanner(System.in);
-        System.out.print("Please make selection: ");
         int selection = 0;
-        while(!fromUser.hasNextInt()){
-            System.out.println("\nInvalid entry please try program again.");
-            return;
-        }
-        selection = fromUser.nextInt();        
         while (selection != 4){
+            System.out.println("\nFinal Project Menu\n");
+            System.out.println("Press 1 for the Wordlist\n");
+            System.out.println("Press 2 for the Doclist\n");
+            System.out.println("Press 3 for Occurs With\n");
+            System.out.println("Press 4 to quit\n");
+        
+            Scanner fromUser = new Scanner(System.in);
+            System.out.print("Please make selection: ");
+        
+            while(!fromUser.hasNextInt()){
+                System.out.println("\nInvalid entry please try program again.");
+                return;
+                }
+            selection = fromUser.nextInt();        
+        
             switch(selection){
                 case 1:{
                 System.out.println("Word list\n");
                 System.out.println("Enter Document ID number (0 to 299): ");
-                while (!fromUser.hasNextInt()){
-                    System.out.println("Invalid entry, please try program again.");
-                    return;
+                if (!fromUser.hasNextInt()){
+                    System.out.println("Invalid entry, restarting menu.");
+                    selection = 0;
+                    break;
                 }
                 int docSelection = fromUser.nextInt();
                 if ((docSelection >= 0) && (docSelection < 300)){
                     System.out.println("Document ID is: " + docSelection);
                     System.out.println("Words contained in Document " + 
                             docSelection +": " + w.getDocList(docSelection));
-                    return;
+                    break;
                 }
                 else{
                     System.out.println("Document number out of range, "
                             + "please try the program again.");
-                    return;
+                    break;
                     }
                 }
             
@@ -72,7 +75,7 @@ public class Driver {
                     System.out.println("Word is not used in any of the "
                             + "documents provided.");
                 }
-                return;
+                break;
                 }
             
                 case 3:{
@@ -82,9 +85,13 @@ public class Driver {
                 System.out.println("Word is: " + wordOccurs);
                 
                 w.occursWith(wordOccurs);
-                return;
+                break;
                 }
-            
+                
+                case 4:{
+                    System.out.println("\nExiting program");
+                    return;
+                }
                 default:{
                     System.out.println("Invalid entry");
                 }
